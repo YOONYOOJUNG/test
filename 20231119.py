e3354ce197b136fca17096d7c71cb4923b0d884f
Yoon <dbsdbwjdtjsrms.naver.com>
2024-06-04 18:15:21 +0900

    first commit

---
+++ 20231119.py
@@ -0,0 +1,32 @@
+# 한 줄 주석 
+
+"""
+여러 줄 주석하는 방법
+[큰 따옴표 3개] (내용) [큰 따옴표 3개]
+"""
+'''
+[작은 따옴표 3개] (내용) [작은 따옴표 3개]
+'''
+
+
+
+# 정수 int
+# 실수 float
+
+#산술연산자
+print(1+1)
+
+#훨씬 더 복잡한 내용을 로딩해서 사용해야 함 > 수학 관련해서 math라는 모듈
+import math
+
+print('math.sqrt(4) : ', math.sqrt(4))
+print('math.pow(4, 2) : ', math.pow(4, 2)) #4를 2번 곱했다 
+
+import random
+print('random.random() : ', random.random()) #랜덤한 숫자를 보여줌
+
+#if 문
+if 5 > 2:
+    print("True")
+    
+
